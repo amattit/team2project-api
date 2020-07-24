885260efd6e6f27ea004ad2a521e2dc2ab6dd301
Михаил Серёгин <seregin.m.andr@sberbank.ru>
2020-07-24 13:15:37 +0300

    Добавлен запрет на добавление одного и рогоже лэйбла два и более раз

--- Sources/App/Controllers/ProjectController/ProjectController+Label.swift
+++ Sources/App/Controllers/ProjectController/ProjectController+Label.swift
@@ -15,14 +15,25 @@ extension ProjectController {
     func addLabelToProject(_ req: Request) throws -> Future<HTTPStatus> {
         let user = try req.requireAuthenticated(User.self)
         return try req.parameters.next(Project.self).flatMap { project in
-            guard try user.requireID() == project.ownerId else {
-                throw Abort(.forbidden, reason: "Только пользователь создавший проект может внести изменения в лэйблы")
-            }
-            return try req.content.decode(AddLabelToProject.self).map { labelDto in
-                return try self.getLabelById(labelDto.labelId, on: req).map { label in
-                    return project.labels.attach(label, on: req)
+            return try project.labels.query(on: req).count().flatMap { count in
+                guard try user.requireID() == project.ownerId else {
+                    throw Abort(.forbidden, reason: "Только пользователь создавший проект может внести изменения в лэйблы")
                 }
-            }.transform(to: HTTPStatus.ok)
+                guard count < 2 else {
+                    throw Abort(.forbidden, reason: "В данный момент можно добавить только 2 метки")
+                }
+                return try req.content.decode(AddLabelToProject.self).flatMap { labelDto in
+                    return try self.getLabelById(labelDto.labelId, on: req).flatMap { label in
+                        
+                        return project.labels.isAttached(label, on: req).flatMap { isAttached in
+                            if !isAttached {
+                                return project.labels.attach(label, on: req).transform(to: .ok)
+                            }
+                            throw Abort(HTTPStatus.found, reason: "Лэйбл уже был добавлен к проекту")
+                        }
+                    }
+                }
+            }
         }
     }
     
