c497a2e99edf1736aa9343f3d053a18064720802
Михаил Серёгин <seregin.m.andr@sberbank.ru>
2020-08-07 20:40:40 +0300

    Поставил фильтр на опубликованные проекты для избранных проектов

--- Sources/App/Controllers/ProjectController/ProjectController+Favorites.swift
+++ Sources/App/Controllers/ProjectController/ProjectController+Favorites.swift
@@ -30,7 +30,7 @@ extension ProjectController {
     /// GET /api/v1/user/favorites/project - только избранные проекты
     func getFavoriteProjects(_ req: Request) throws -> Future<[ProjectListResponse]> {
         let user = try req.requireAuthenticated(User.self)
-        return try user.favoritesProjects.query(on: req).all().flatMap { projects in
+        return try user.favoritesProjects.query(on: req).filter(\.isPublished, .equal, 1).all().flatMap { projects in
             return projects.map { project in
                 return project.user.query(on: req).first().unwrap(or: Abort(.badRequest)).map { user in
                     return try ProjectListResponse(with: project, and: user)
